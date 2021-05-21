3d77dd304e884f9621fe1bcd4106a75e840c8d34
snavti <navti@hotmail.com>
2021-05-21 15:46:49 +0100

    Removed XP

--- enrol/xp/ui/src/components/Input.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ className = '', ...props }) => {
-  /** Apply those classes for normalised styling across themes and versions. */
-  return <input {...props} className={`xp-m-0 xp-h-auto form-control ${className}`} />;
-};
-
-export default Input;
